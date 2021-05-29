6e193ebf6395c347adce85728d05f590ededcb56
k1LoW <k1lowxb@gmail.com>
2021-05-30 00:02:25 +0900

    Add test

--- config/config.go
+++ config/config.go
@@ -382,3 +382,29 @@ func CheckIf(cond string) (bool, error) {
 	}
 	return ok.(bool), nil
 }
+
+func traverseGitPath(base string) (string, error) {
+	p, err := filepath.Abs(base)
+	if err != nil {
+		return "", err
+	}
+	for {
+		fi, err := os.Stat(p)
+		if err != nil {
+			return "", err
+		}
+		if !fi.IsDir() {
+			p = filepath.Dir(p)
+			continue
+		}
+		gitConfig := filepath.Join(p, ".git", "config")
+		if fi, err := os.Stat(gitConfig); err == nil && !fi.IsDir() {
+			return p, nil
+		}
+		if p == "/" {
+			break
+		}
+		p = filepath.Dir(p)
+	}
+	return "", fmt.Errorf("failed to traverse the Git root path: %s", base)
+}
--- config/config_test.go
+++ config/config_test.go
@@ -170,6 +170,46 @@ func TestTestExecutionTimeAcceptable(t *testing.T) {
 	}
 }
 
+func TestTraverseGitPath(t *testing.T) {
+	dir := t.TempDir()
+	if err := os.MkdirAll(filepath.Join(dir, "a", "b", "c", "d"), 0700); err != nil {
+		t.Fatal(err)
+	}
+	if err := os.Mkdir(filepath.Join(dir, "a", "b", ".git"), 0700); err != nil {
+		t.Fatal(err)
+	}
+	f, err := os.Create(filepath.Join(dir, "a", "b", ".git", "config"))
+	if err != nil {
+		t.Fatal(err)
+	}
+	f.Close()
+
+	tests := []struct {
+		base    string
+		wantErr bool
+	}{
+		{filepath.Join(dir, "a", "b", "c"), false},
+		{filepath.Join(dir, "a", "b", "c", "d"), false},
+		{filepath.Join(dir, "a", "b"), false},
+		{filepath.Join(dir, "a"), true},
+	}
+	for _, tt := range tests {
+		got, err := traverseGitPath(tt.base)
+		if err != nil {
+			if !tt.wantErr {
+				t.Errorf("got %v\nwantErr %v", err, tt.wantErr)
+			}
+		} else {
+			if tt.wantErr {
+				t.Errorf("got %v\nwantErr %v", nil, tt.wantErr)
+			}
+			if want := filepath.Join(dir, "a", "b"); got != want {
+				t.Errorf("got %v\nwant %v", got, want)
+			}
+		}
+	}
+}
+
 func revertEnv(envCache []string) error {
 	if err := clearEnv(); err != nil {
 		return err
