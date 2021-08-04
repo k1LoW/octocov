f366acd3b7bafbdfa99d24247cc98dfda8d58666
k1LoW <k1lowxb@gmail.com>
2021-08-04 14:53:08 +0900

    Add interface checker

--- datastore/datastore.go
+++ datastore/datastore.go
@@ -7,6 +7,13 @@ import (
 	"github.com/k1LoW/octocov/report"
 )
 
+var (
+	_ Datastore = (*Github)(nil)
+	_ Datastore = (*S3)(nil)
+	_ Datastore = (*GCS)(nil)
+	_ Datastore = (*BQ)(nil)
+)
+
 type Datastore interface {
 	Store(ctx context.Context, path string, r *report.Report) error
 	FS(path string) (fs.FS, error)
