3e59a201ce412bd92b47057b0bfdd18fc2fd43fe
k1LoW <k1lowxb@gmail.com>
2021-09-15 16:39:54 +0900

    Flush the block coverages from the report to handle `Error 413 (Request Entity Too Large)!!1` error.

--- report/report_test.go
+++ report/report_test.go
@@ -37,6 +37,9 @@ func TestTable(t *testing.T) {
 		if got := r.Table(); got != tt.want {
 			t.Errorf("got\n%v\nwant\n%v", got, tt.want)
 		}
+		_ = r.String()
+		r.Coverage.FlushBlockCoverages()
+		_ = r.String()
 	}
 }
 
