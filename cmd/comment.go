e2ad260fc1631aa34a94ea2ed26ef0f9fe6d4020
k1LoW <k1lowxb@gmail.com>
2021-09-17 10:15:41 +0900

    Refactor code

---
+++ cmd/comment.go
@@ -0,0 +1,45 @@
+package cmd
+
+import (
+	"context"
+	"strings"
+
+	"github.com/k1LoW/octocov/config"
+	"github.com/k1LoW/octocov/gh"
+	"github.com/k1LoW/octocov/report"
+)
+
+func commentReport(ctx context.Context, c *config.Config, r *report.Report) error {
+	owner, repo, err := gh.SplitRepository(c.Repository)
+	if err != nil {
+		return err
+	}
+	gh, err := gh.New()
+	if err != nil {
+		return err
+	}
+	n, err := gh.DetectCurrentPullRequestNumber(ctx, owner, repo)
+	if err != nil {
+		return err
+	}
+	files, err := gh.GetPullRequestFiles(ctx, owner, repo, n)
+	if err != nil {
+		return err
+	}
+	footer := "Reported by [octocov](https://github.com/k1LoW/octocov)"
+	if c.Comment.HideFooterLink {
+		footer = "Reported by octocov"
+	}
+	comment := strings.Join([]string{
+		"## Code Metrics Report",
+		r.Table(),
+		"",
+		r.FileCoveagesTable(files),
+		"---",
+		footer,
+	}, "\n")
+	if err := gh.PutComment(ctx, owner, repo, n, comment); err != nil {
+		return err
+	}
+	return nil
+}
